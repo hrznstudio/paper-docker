fac95694a481ba1bece7e6323b6ee349cf465aaa
Coded <coded@hrznstudio.com>
2019-08-28 22:02:41 +0100

    :sparkles: Add QueryPort variable

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ set -e
 
 sed -i "s/{{MOTD}}/&1${HOSTNAME}/" /paper/server.properties
 sed -i "s/{{PORT}}/&1${PORT}/" /paper/server.properties
+sed -i "s/{{QUERYPORT}}/&1${QUERYPORT}/" /paper/server.properties
 
 COMMAND="java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY $JAVA_ARGS -jar Paper.jar"
 echo "Running Paper - $COMMAND"
