8e9e351485f92a767d828aa44f904116d44bc9b0
Coded <coded@hrznstudio.com>
2019-08-28 22:02:57 +0100

    :sparkles: Add PlayerCount variable

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -e
 sed -i "s/{{MOTD}}/&1${HOSTNAME}/" /paper/server.properties
 sed -i "s/{{PORT}}/&1${PORT}/" /paper/server.properties
 sed -i "s/{{QUERYPORT}}/&1${QUERYPORT}/" /paper/server.properties
+sed -i "s/{{PLAYERCOUNT}}/&1${PLAYERCOUNT}/" /paper/server.properties
 
 COMMAND="java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY $JAVA_ARGS -jar Paper.jar"
 echo "Running Paper - $COMMAND"
